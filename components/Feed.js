46d5c20a40b0dc83edc213a8ae57dab07fcca550
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-12 01:58:30 +0300

    create asession from the header

--- components/Feed.js
+++ components/Feed.js
@@ -1,12 +1,15 @@
 import Stories from "./Stories"
 import Posts from "./Posts"
 import Miniprofile from "./Miniprofile"
-import Suggestions from "./Suggestions"
+import Suggestions from "./Suggestions";
+import { useSession } from 'next-auth/react';
 
 
 function Feed() {
+
+  const {data:session} = useSession();
   return (
-    <main className="grid grid-cols-1  md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:mx-w-6xl mx-auto">
+    <main className={`grid grid-cols-1  md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:mx-w-6xl mx-auto ${!session && "!grid-cols-1 !max-w-3xl"}`}>
 
          {/* section*/}
          <section className="col-span-2">
@@ -18,7 +21,8 @@ function Feed() {
             
 
         {/* section*/}
-        <section className="hidden xl:inline-grid md:col-span-1">
+        {session && (
+            <section className="hidden xl:inline-grid md:col-span-1">
             <div className="fixed top-20">
                  {/* mini profile*/}
             <Miniprofile/>
@@ -28,6 +32,8 @@ function Feed() {
             </div>
            
         </section>
+        )}
+        
             
         
     </main>
