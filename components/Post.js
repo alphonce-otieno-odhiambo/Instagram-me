c08acc3747e38680e2ac87e169af0ee6026f31d4
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-11 13:39:20 +0300

    style post header

--- components/Post.js
+++ components/Post.js
@@ -11,13 +11,13 @@ import {
   import { HeartIcon as HeartIconFilled} from "@heroicons/react/solid"
 function Post({id,username,userImg,img ,caption}) {
   return (
-    <div>
-        <h1>I im a post</h1>
+    <div className='bg-white my-7 border p-0 '>
+       
         {/**header */}
         <div className='flex items-center p-5'>
             <img className='object-contain rounded-full h-12 w-12 border p-1 mr-3'
              src={userImg} alt=''/>
-            <p>{username} </p>
+            <p className='flex-1 font-bold'>{username} </p>
             <DotsHorizontalIcon className='h-5'/>
         </div>
         {/**image */}
