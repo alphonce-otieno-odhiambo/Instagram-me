b5ba7cbc73e3160fc379c5f37fe1c95dcfca3b06
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-12 01:32:25 +0300

    create session

--- components/Miniprofile.js
+++ components/Miniprofile.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import {signOut, useSession} from "next-auth/react"
 
 function Miniprofile() {
+
+      const {data:session} = useSession();
+      console.log(session);
+
   return (
     <div className='flex items-center justify-between mt-12 ml-10'>
         <img className='object-contain rounded-full h-16 w-16 border p-[2px] '
-             src="https://links.papareact.com/3ke" alt=''/>
+             src={session?.user?.image} alt=''/>
 
              <div className='flex-1 mx-4'>
-                 <h2 className='font-bold'>Alphonce</h2>
+                 <h2 className='font-bold'>{session?.user?.username}</h2>
                  <h4 className='text-sm text-gray-400'>Welcome to instagram me</h4>
              </div>
-             <button className='text-blue-400 text-sm font-semibold'>Sign out</button>
+             <button onClick={signOut} className='text-blue-400 text-sm font-semibold'>Sign out</button>
     </div>
   )
 }
