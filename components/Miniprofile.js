9eab8fcffabb0d91d069777e5b5d9eedd72319bb
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-11 15:59:57 +0300

    use hooks to fetch data

--- components/Miniprofile.js
+++ components/Miniprofile.js
@@ -5,12 +5,12 @@ function Miniprofile() {
     <div className='flex items-center justify-between mt-12 ml-10'>
         <img className='object-contain rounded-full h-16 w-16 border p-[2px] '
              src="https://links.papareact.com/3ke" alt=''/>
-             
-             <div>
-                 <h2>Alphonce</h2>
-                 <h4>Welcome to instagram me</h4>
+
+             <div className='flex-1 mx-4'>
+                 <h2 className='font-bold'>Alphonce</h2>
+                 <h4 className='text-sm text-grey-400'>Welcome to instagram me</h4>
              </div>
-             <button>Sign out</button>
+             <button className='text-blue-400 text-sm font-semibold'>Sign out</button>
     </div>
   )
 }
--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import faker from '@faker-js/faker';
 
 function Suggestions() {
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+     const suggestions = [...Array(5)].map((_, i)=>[{
+         ...faker.helpers.contextualCard(),
+         id:i
+     }])
+     setSuggestions(suggestions);
+    }, [])
+    
   return (
     <div>Suggestions</div>
   )
