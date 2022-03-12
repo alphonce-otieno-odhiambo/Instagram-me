152c90c86b2c58dc4260212332a8c51eb339febc
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-12 05:34:25 +0300

    import useRef and Fragment

--- components/Modal.js
+++ components/Modal.js
@@ -2,9 +2,12 @@ import React, { Fragment } from 'react'
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modalAtom';
 import { Dialog, Transition} from '@headlessui/react'
+import { CameraIcon } from '@heroicons/react/outline';
+import { Fragment, useRef} from 'react'
 
 function Modal() {
     const [open, setOpen] = useRecoilState(modalState);
+    
   return (
     <Transition.Root show={open} as={Fragment} >
             <Dialog as='div' className="fixed z-10 inset-0 overflow-y-auto" 
@@ -40,6 +43,13 @@ function Modal() {
                 >
                     <div className='inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left  overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6'>
                         <div >
+                            <div
+                            //onClick={() => filePikerRef.current.click()}
+                            className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100 cursor-pointer"
+                            >
+                                <CameraIcon className='h-6 w-6 text-red-600'
+                                aria-hidden="true"/>
+                            </div>
                             <div>
                                 <div className='mt-3 text-center sm:mt-5'>
                                     <Dialog.Title 
