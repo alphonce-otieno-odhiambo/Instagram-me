63afc22cc002462e8ff825def26d3c40cd570da5
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-12 03:53:49 +0300

    instimport Dialog & Transiton from headlessui

--- components/Modal.js
+++ components/Modal.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modalAtom';
+import { Dialog, Transition} from '@headlessui/react'
 
 function Modal() {
     const [open, setOpen] = useRecoilState(modalState);
   return (
-    <div>
-        <h1></h1>
-    </div>
+    <Transition.Root show={open} as={Fragment} >
+            <Dialog as='div' className="fixed z-10 inset-0 overflow-y-auto" 
+            onClose={setOpen}
+            >
+            <h1>Hello ali</h1>
+            </Dialog>
+    </Transition.Root>
   )
 }
 
