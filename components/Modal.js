2f99cd143ae8984e9e74d37898daeaff7fc50aaa
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-12 03:58:31 +0300

    create the Modal & style it

--- components/Modal.js
+++ components/Modal.js
@@ -10,7 +10,9 @@ function Modal() {
             <Dialog as='div' className="fixed z-10 inset-0 overflow-y-auto" 
             onClose={setOpen}
             >
-            <h1>Hello ali</h1>
+            <div className='flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
+
+            </div>
             </Dialog>
     </Transition.Root>
   )
